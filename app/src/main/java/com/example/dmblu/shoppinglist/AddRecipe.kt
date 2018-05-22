67fe153f08fbaeba81357489fd6bf43a547cf3d9
David Meyer <dmeyer31@gatech.edu>
2018-05-22 17:02:46 -0400

    home screen basic layout

---
+++ app/src/main/java/com/example/dmblu/shoppinglist/AddRecipe.kt
@@ -0,0 +1,12 @@
+package com.example.dmblu.shoppinglist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddRecipe : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_recipe)
+    }
+}
--- app/src/main/java/com/example/dmblu/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/dmblu/shoppinglist/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.dmblu.shoppinglist
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /*
+        Function that goes into the form to add a recipe to database
+     */
+    fun addRecipe(view: View) {
+        val randomIntent = Intent(this, AddRecipe::class.java)
+        startActivity(randomIntent)
     }
 }
